a5a849ee8e867b7ee8dd711bd74b3a798e1c77ae
akbaralinabiev <akbaralinabiev14@gmail.com>
2023-01-09 04:06:41 +0100

    added input for user

--- game.py
+++ game.py
@@ -1,4 +1,7 @@
-for number in range(1, 101):
+
+num = int(input("Enter a number: "))
+
+for number in range(1, num+1):
     if number % 3 == 0 and number % 5 ==0:
     # if number in range is divisible by 3 and 5, in output in prints "FizzBuzz" instead of number
          print("FizzBuzz")
